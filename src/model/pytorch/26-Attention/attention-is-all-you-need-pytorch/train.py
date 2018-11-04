d5f6b12f6bdfb29532b65968f9ef82ec37269039
inessus <inessus@163.com>
2018-11-05 06:53:25 +0800

    start

--- src/model/pytorch/26-Attention/attention-is-all-you-need-pytorch/train.py
+++ src/model/pytorch/26-Attention/attention-is-all-you-need-pytorch/train.py
@@ -16,6 +16,7 @@ from dataset import TranslationDataset, paired_collate_fn
 from transformer.Models import Transformer
 from transformer.Optim import ScheduledOptim
 
+
 def cal_performance(pred, gold, smoothing=False):
     ''' Apply label smoothing if needed '''
 
@@ -92,6 +93,7 @@ def train_epoch(model, training_data, optimizer, device, smoothing):
     accuracy = n_word_correct/n_word_total
     return loss_per_word, accuracy
 
+
 def eval_epoch(model, validation_data, device):
     ''' Epoch operation in evaluation phase '''
 
@@ -125,6 +127,7 @@ def eval_epoch(model, validation_data, device):
     loss_per_word = total_loss/n_word_total
     accuracy = n_word_correct/n_word_total
     return loss_per_word, accuracy
+
 
 def train(model, training_data, validation_data, optimizer, device, opt):
     ''' Start training '''
